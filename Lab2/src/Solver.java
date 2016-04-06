4ca05874e0b154ae24b0b4bbd536454cb1131d2c
mas13fbj@student.lu.se <mas13fbj@hacke-10.student.lth.se>
2016-04-06 10:00:00 +0200

    Färdiga versionen av lab2

--- Lab2/src/Solver.java
+++ Lab2/src/Solver.java
@@ -5,24 +5,12 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Solver {
-	////// DIRECTED GRAPHS!!!///////
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	// !!
-	//
 
 	private Scanner scan;
 	private HashMap<String, ArrayList<String>> graph;
 	private ArrayDeque<String> que;
-	private boolean state;
 	private ArrayList<String> visited;
 	private HashMap<String, Integer> lengthTo;
-	
 
 	public Solver(String path, HashMap<String, ArrayList<String>> graph) {
 		this.graph = graph;
@@ -32,7 +20,7 @@ public class Solver {
 			scan.close();
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.out.println("File not found");
+			System.out.println("File not found eller bara ett random fel i hela resten av programmet!");
 		}
 	}
 
@@ -41,51 +29,41 @@ public class Solver {
 			String root = scan.next();
 			String target = scan.next();
 			que = new ArrayDeque<String>();
-			
-			int temp = solve(root, target);
-			if(temp > 0){
-				System.out.println(temp);
-			}else{
-				System.out.println(-1);
-			}
-			
+			solve(root, target);
 		}
 	}
 
-	private int solve(String root, String target) {
-		lengthTo = new HashMap<String, Integer>();
-		visited = new ArrayList<String>();
-		visited.add(root);
-		lengthTo.put(root, 0);
-		que.push(root);
-		while(!que.isEmpty()){
-			String node = que.pop();
-			for(String string : graph.get(node)){
-				if(string == target){
-					return lengthTo.get(node) + 1;
-				}else if(!visited.contains(string)){
-					lengthTo.put(string, lengthTo.get(node) + 1);
-					visited.add(string);
-					que.push(string);
-				}
+	private void solve(String root, String target) {
+		boolean found = false;									
+		int depth = 0;					
+		if (root.compareTo(target) == 0) {						
+			System.out.println(depth);
+		} else {
+			lengthTo = new HashMap<String, Integer>();
+			visited = new ArrayList<String>();
+			visited.add(root);
+			lengthTo.put(root, 0);
+			que.push(root);
+			while (!found && !que.isEmpty()) {
+				String node = que.poll();						
+				for (String string : graph.get(node)) {			
+					if (!visited.contains(string)) {			
+						lengthTo.put(string, lengthTo.get(node) + 1);
+						visited.add(string);
+						que.addLast(string);																				
+					}											
+					if (string.compareTo(target) == 0) {		
+						found = true;
+						depth = lengthTo.get(target);
+						break;									
+					}											
+				}																																	
+			}
+			if (depth == 0) {
+				System.out.println("-1");						
+			} else {											
+				System.out.println(depth);
 			}
 		}
-		return lengthTo.get(target);
-//		if (!visited.contains(root)) {
-//			que.add(root);
-//			visited.add(root);
-//			while (!que.isEmpty()) {
-//				ArrayList<String> list = graph.get(que.pop());
-//				int counter = 0;
-//				for (String string : list) {
-//					int temp = solve(, target, depth + 1);
-//					if(temp < counter){
-//						counter = temp;
-//					}
-//				}
-//			}
-//		}
-
 	}
-
 }
--- Lab2/src/WordLadder.java
+++ Lab2/src/WordLadder.java
@@ -1,6 +1,6 @@
 
 public class WordLadder {
-
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Builder builder = new Builder(args[0]);
 		Solver solver = new Solver(args[1], builder.getGraph());
